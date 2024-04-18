1f82d23177400abe7ed114fb1faa2d7ce1c934a8
Luis <102195108+P0cketbear@users.noreply.github.com>
2024-04-17 23:47:57 -0400

    back it up

--- base/models.py
+++ base/models.py
@@ -16,8 +16,8 @@ class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     description = models.TextField(blank=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
